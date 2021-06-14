b30a11c877c392840be5990001a347831f79fe47
Luis Borjas Reyes <luisfborjas@gmail.com>
2021-06-13 20:33:55 -0400

    Make it a bit more general

--- TransitCharts.hs
+++ TransitCharts.hs
@@ -1,15 +1,67 @@
 {-# LANGUAGE NamedFieldPuns #-}
 module TransitCharts (main) where
 import SwissEphemeris
+    ( calculateEclipticPosition,
+      gregorianDateTime,
+      julianDay,
+      EclipticPosition(EclipticPosition, lng),
+      JulianTime(..),
+      Planet(Chiron, Moon, Mercury, Venus, Mars, Jupiter, Saturn, Uranus,
+             Neptune, Sun, Pluto, MeanNode, MeanApog) )
 import Data.Time
+    ( Day,
+      UTCTime(UTCTime),
+      fromGregorian,
+      toGregorian,
+      diffTimeToPicoseconds,
+      picosecondsToDiffTime )
 import Data.Bifunctor (bimap)
-import Control.Lens
-import Data.Foldable
-import Data.Maybe (catMaybes)
+import Control.Lens ( (.=) )
+import Data.Foldable ( forM_, traverse_ )
+import Data.Maybe (catMaybes, fromJust, isJust)
 import Graphics.Rendering.Chart.Easy
-import Graphics.Rendering.Chart.Backend.Diagrams
+    ( line_color,
+      line_dashes,
+      solidFillStyle,
+      layout_title,
+      plot_fillbetween_style,
+      plot_fillbetween_title,
+      plot_fillbetween_values,
+      plot_lines_style,
+      plot_lines_title,
+      plot_lines_values,
+      Layout,
+      PlotFillBetween,
+      PlotLines,
+      Default(def),
+      AlphaColour,
+      Colour,
+      liftEC,
+      plot,
+      opaque,
+      withOpacity,
+      darkblue,
+      darkgreen,
+      darkolivegreen,
+      darkorange,
+      darkorchid,
+      darkred,
+      darkslateblue,
+      darkviolet,
+      deeppink,
+      dimgray,
+      firebrick,
+      green,
+      orange,
+      purple,
+      red,
+      salmon,
+      EC )
+import Graphics.Rendering.Chart.Backend.Diagrams ( toFile )
 import Control.Monad.IO.Class (liftIO)
 import Data.Traversable (forM)
+import Data.Either (fromRight)
+import Data.Time.Format.ISO8601 (iso8601ParseM)
 
 type EclipticPoint = (UTCTime, Double)
 type Ephemeris = (Planet, [EclipticPoint])
@@ -24,31 +76,32 @@ defaultOrb = 1.0
 
 
 main :: IO ()
-main =
-  traverse_ transitChart transited
-  where
-    transited = natalPlanets --filter ((==) Uranus . fst) natalPlanets
+main = do
+  let days = julianDays (fromGregorian 2021 1 1) (fromGregorian 2022 1 1)
+  bday <- iso8601ParseM "1989-06-07T05:30:00Z"
+  transited <- mapM (\p -> natalPositions p bday days) defaultPlanets
+  traverse_ (transitChart days) transited
 
-transitChart :: Ephemeris -> IO ()
-transitChart natalEphe@(transited, natalPositions) =  do
+transitChart :: [JulianTime] -> Ephemeris -> IO ()
+transitChart transitRange natalEphe@(transited, natalPositions) =  do
   transitingPositions <-
-    forM (map fst natalPlanets) $ \transiting -> do
-      transitingPositions transiting
+    forM defaultPlanets $ \transiting -> do
+      transitingPositions transiting transitRange
 
   toFile def ("charts/" <> show transited <> "_transits.svg") $ do
     layout_title .= "Transits to " <> show transited
     -- plot the positions of all planets for all year
     forM_ transitingPositions $ \(transiting, ephemeris) -> do
       plot $
-        planetLine 
-          (show transiting) 
-          (opaque $ planetColor transiting) 
+        planetLine
+          (show transiting)
+          (opaque $ planetColor transiting)
           (planetLineStyle transiting)
           [ephemeris]
     -- plot the original natal position line
     plot $
-      planetLine 
-        (show transited) 
+      planetLine
+        (show transited)
         (opaque green)
         Dotted
         [natalPositions]
@@ -82,7 +135,7 @@ planetLine :: String
 planetLine title color lineStyle values = liftEC $ do
   plot_lines_title .= title
   plot_lines_values .= values
-  plot_lines_style . line_color .= color 
+  plot_lines_style . line_color .= color
   plot_lines_style . line_dashes .= dashes lineStyle
   where
     dashes Solid = []
@@ -102,59 +155,46 @@ planetColor Moon = dimgray
 planetColor Mercury = purple
 planetColor Venus = salmon
 planetColor Mars = red
-planetColor Jupiter = darkviolet 
+planetColor Jupiter = darkviolet
 planetColor Saturn = darkred
-planetColor Uranus = darkolivegreen 
-planetColor Neptune = darkslateblue 
-planetColor Pluto = deeppink 
-planetColor MeanNode = dimgray 
-planetColor MeanApog = darkorchid 
-planetColor Chiron = firebrick 
+planetColor Uranus = darkolivegreen
+planetColor Neptune = darkslateblue
+planetColor Pluto = deeppink
+planetColor MeanNode = dimgray
+planetColor MeanApog = darkorchid
+planetColor Chiron = firebrick
 
 -- | Get all days in the current year, as @JulianTime@s
-currentYearDays :: [JulianTime]
-currentYearDays =
+julianDays :: Day -> Day -> [JulianTime]
+julianDays startDay endDay =
   map JulianTime [start, (start + 1.0) .. end]
   where
-    start = unJulianTime . utcToJulian $ UTCTime (fromGregorian 2021 1 1) 0
-    end = unJulianTime . utcToJulian $ UTCTime (fromGregorian 2021 12 31) 0
+    start = unJulianTime . utcToJulian $ UTCTime startDay 0
+    end = unJulianTime . utcToJulian $ UTCTime endDay 0
 
-transitingPositions :: Planet -> IO Ephemeris
-transitingPositions p = do
-  ephe <- mapM mkEphemeris currentYearDays
+transitingPositions :: Planet -> [JulianTime] -> IO Ephemeris
+transitingPositions p days = do
+  ephe <- mapM (eclipticEphemeris p) days
   pure (p, catMaybes ephe)
+  
+natalPositions :: Planet -> UTCTime -> [JulianTime] -> IO Ephemeris
+natalPositions p bday days = do
+  ephe <- eclipticEphemeris p (utcToJulian bday)
+  case ephe of
+    Nothing -> pure (p, [])
+    Just (_t, pos) -> pure (p, [(julianToUTC d, pos) | d <- days])
 
-  where
-    mkEphemeris :: JulianTime -> IO (Maybe (UTCTime, Double))
-    mkEphemeris t = do
-      pos <- calculateEclipticPosition t p
-      case pos of
-        Left e -> pure Nothing
-        Right EclipticPosition{lng} -> pure $ Just (julianToUTC t,lng)
 
--- | Natal positions for a single person
-natalPlanets :: [Ephemeris]
-natalPlanets =
-  [
-    (Sun, allYear 286.90)
-  , (Moon, allYear 279.38)
-  , (Mercury, allYear 305.83)
-  , (Venus, allYear 265.59)
-  , (Mars, allYear 23.43)
-  , (Jupiter, allYear 56.38)
-  , (Saturn, allYear 276.32)
-  , (Uranus, allYear 272.12)
-  , (Neptune, allYear 280.16)
-  , (Pluto, allYear 224.71)
-  , (MeanNode, allYear 337.46)
-  , (MeanApog, allYear 176.41)
-  , (Chiron, allYear 93.46)
-  ]
-  where
-    allYear pos = [(julianToUTC d, pos) | d <- currentYearDays]
----
---- HELPERS
----
+eclipticEphemeris :: Planet -> JulianTime -> IO (Maybe (UTCTime, Double))
+eclipticEphemeris p t = do
+  pos <- calculateEclipticPosition t p
+  case pos of
+    Left e -> pure Nothing
+    Right EclipticPosition{lng} -> pure $ Just (julianToUTC t,lng)
+
+defaultPlanets :: [Planet]
+defaultPlanets = [Sun .. Pluto] <> [MeanNode, MeanApog, Chiron]
+
 
 picosecondsInHour :: Double
 picosecondsInHour = 3600 * 1e12
