3631033bd38b9ae39b2620ab58cf7518c72230dd
Luis Borjas Reyes <luisfborjas@gmail.com>
2021-08-25 23:58:02 -0400

    Adapt crossings

--- src/Query/Crossing.hs
+++ src/Query/Crossing.hs
@@ -18,73 +18,9 @@ import qualified Streaming.Prelude as St
 import Control.Category ((>>>))
 import Query.Streaming
 import Query.Common (concatForEach)
-
--- TODO: remove this?
-data SimplePlanetStation = Retrograde | Direct
-  deriving (Eq, Show)
-
-data Crossing a = Crossing
-  { crossingEnters :: !JulianDayTT
-  , crossingExits :: !JulianDayTT
-  , crossingSubject :: !a
-  , crossingPlanetEntered :: !(Maybe SimplePlanetStation)
-  , crossingPlanetExited :: !(Maybe SimplePlanetStation)
-  } deriving (Show)
-
-instance Eq a => Merge (Crossing a) where
-  x `merge` y =
-    if crossingSubject x == crossingSubject y then
-      Merge merged
-    else
-      ReplaceL updated
-    where
-      merged = x {
-        crossingExits = crossingExits y,
-        crossingPlanetExited = crossingPlanetExited y
-      }
-      updated = x {
-        crossingExits = crossingEnters y,
-        crossingPlanetExited = crossingPlanetEntered y
-      }
-      
-instance Temporal (Crossing a) where
-  type TemporalIndex (Crossing a) = JulianDayTT
-  startTime = crossingEnters
-  endTime = crossingExits
+import Query.EventTypes
 
 type CrossingMap a = Grouped Planet (Crossing a)
-
-data Zodiac = Zodiac
-  { signName :: ZodiacSignName, signLng :: Double}
-  deriving (Eq, Show)
-
-instance HasEclipticLongitude Zodiac where
-  getEclipticLongitude (Zodiac _ l) = l
-
-data HouseName
-  = I
-  | II
-  | III
-  | IV
-  | V
-  | VI
-  | VII
-  | VIII
-  | IX
-  | X
-  | XI
-  | XII
-  deriving (Eq, Show, Enum, Ord)
-
-data House = House
-  { houseName :: HouseName, houseCusp :: Double }
-  deriving (Eq, Show)
-  
-instance HasEclipticLongitude House where
-  getEclipticLongitude = houseCusp
-
-instance Ord House where
-  a `compare` b = compare (houseCusp a) (houseCusp b)
 
 crossings :: (Monad m, HasEclipticLongitude a) => [a] -> Stream (Of (Ephemeris Double)) m b -> m (Of (CrossingMap a) b)
 crossings degs =
@@ -93,6 +29,8 @@ crossings degs =
 mapCrossings :: HasEclipticLongitude a => [a] -> Seq (Ephemeris Double) -> CrossingMap a
 mapCrossings degreesToCross (pos1 :<| pos2 :<| _) =
   concatForEach (zip (toList $ ephePositions pos1) (toList $ ephePositions pos2)) $ \(p1, p2) ->
+    -- TODO: allow selecting which planets to consider -- we _do_ want to know
+    -- Moon crossings over house cusps.
     -- the Moon crosses most of the ecliptic every month, so it's not
     -- really significant. The nodes do have a slower behavior, so maybe
     -- I'll reinstate them.
@@ -107,15 +45,35 @@ mapCrossings degreesToCross (pos1 :<| pos2 :<| _) =
 
 mapCrossings _ _ = mempty
 
+getCrossings' :: HasEclipticLongitude a => (Crossing a -> Event ) -> [Planet] -> [a] -> Seq (Ephemeris Double) -> Grouped Planet Event
+getCrossings' mkEvent selectedPlanets degreesToCross (pos1 :<| pos2 :<| _) =
+  concatForEach (zip (toList $ ephePositions pos1) (toList $ ephePositions pos2)) $ \(p1, p2) ->
+    if ephePlanet p1 `notElem` selectedPlanets then
+      mempty
+    else
+      Aggregate
+        $ M.fromList
+        $ map (\c -> (ephePlanet p1, singleton $ mkEvent c))
+        $ mapMaybe (mkCrossing (epheDate pos1, p1) (epheDate pos2, p2))
+        degreesToCross
+
+getCrossings' _ _ _ _ = mempty
+
+getZodiacCrossings :: [Planet]-> [Zodiac] -> Seq (Ephemeris Double) -> Grouped Planet Event
+getZodiacCrossings = getCrossings' ZodiacIngress
+
+getHouseCrossings :: [Planet] -> [House] -> Seq (Ephemeris Double) -> Grouped Planet Event
+getHouseCrossings = getCrossings' HouseIngress 
+
 mkCrossing :: HasEclipticLongitude a => (JulianDayTT, EphemerisPosition Double) -> (JulianDayTT, EphemerisPosition Double) -> a -> Maybe (Crossing a)
 mkCrossing (d1, pos1) (d2, pos2) toCross
   | crossesDirect (epheLongitude pos1) (epheLongitude pos2) (getEclipticLongitude toCross) =
      Just $ Crossing {
-        crossingEnters = d1,
-        crossingExits = d2,
-        crossingSubject = toCross,
-        crossingPlanetEntered = Just Direct,
-        crossingPlanetExited = Nothing
+        crossingStarts = d1,
+        crossingEnds = d2,
+        crossingCrosses = toCross,
+        crossingPlanet = ephePlanet pos1,
+        crossingDirection = if epheSpeed pos2 < 0 then RetrogradeMotion  else DirectMotion
       }
   | otherwise = Nothing
 
