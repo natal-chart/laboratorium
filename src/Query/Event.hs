edead979058ed40c9fa34fec210b1d1874cb3803
Luis Borjas Reyes <luisfborjas@gmail.com>
2021-10-18 19:38:23 -0400

    Give up on other moon phases for now
    Will probably have to look into phases _changing_ vs. persisting

--- src/Query/Event.hs
+++ src/Query/Event.hs
@@ -23,8 +23,13 @@ eventExactAt (DirectionChange PlanetStation{stationStarts, stationEnds, stationP
     Left _e -> pure []
     Right (dirChangesAt, _) -> mapM fromJulianDay [dirChangesAt]
 eventExactAt (LunarPhaseChange LunarPhase{lunarPhaseName, lunarPhaseStarts, lunarPhaseEnds}) =
-  moonPhaseExactAt lunarPhaseName lunarPhaseStarts lunarPhaseEnds
-  >>= crossingAsList
+  if lunarPhaseName `elem` [FullMoon, NewMoon] then
+    moonPhaseExactAt lunarPhaseName lunarPhaseStarts lunarPhaseEnds
+    >>= crossingAsList
+  else
+    -- we don't currently interpolate other moon phases, as the brackets
+    -- are too large and sometimes we can't converge in time.
+    pure mempty
 eventExactAt (EclipseMaximum ecl) =
   mapM fromJulianDay [getEclipseDate ecl]
 eventExactAt (PlanetaryTransit t) = transitExactAt t
